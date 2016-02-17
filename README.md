c9235866ba8f9d21afba413efa2fbacb32c3f7b5
Jean Faubert Dorancy <faubertdor@gmail.com>
2016-02-16 20:17:23 -0500

    Email set for Heroku

--- README.md
+++ README.md
@@ -2,4 +2,6 @@ Learning Rails
 ==
 
 Learning Rails with a tutorial from the RailsApps project.
+The app is hosted on Heroku and can be found following the link below.
+Link: http://mailing-list-jdorancy.herokuapp.com/
 
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -42,7 +42,7 @@ Rails.application.configure do
   # config.action_dispatch.x_sendfile_header = 'X-Accel-Redirect' # for NGINX
 
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
-  # config.force_ssl = true
+  config.force_ssl = true
 
   # Use the lowest log level to ensure availability of diagnostic information
   # when problems arise.
@@ -76,4 +76,17 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+  
+  # email enabled in production
+  config.action_mailer.default_url_options = { :host => Rails.application.secrets.domain_name }
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.smtp_settings = {
+  address: "smtp.mandrillapp.com",
+  port: 587,
+  user_name: Rails.application.secrets.email_provider_username,
+  password: Rails.application.secrets.email_provider_api_key
+  }
+
 end
