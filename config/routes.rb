a77026ec7cd5b435fcfea37c3f24045ffd1ace7b
Jean Faubert Dorancy <faubertdor@gmail.com>
2016-02-16 19:44:41 -0500

    Mailing list

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   root to: 'visitors#new'
   resources :contacts, only: [:new, :create]
-  
+  resources :visitors, only: [:new, :create]
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
